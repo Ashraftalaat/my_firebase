03d193b67e5844a5980a03475c247d8054eb7389
Your NameAshraftalaat <atslook@gmail.com>
2024-01-27 01:09:36 +0200

    stream

--- my_firebase/lib/note/filter.dart
+++ my_firebase/lib/note/filter.dart
@@ -9,43 +9,49 @@ class FilterFirestore extends StatefulWidget {
 }
 
 class _FilterFirestoreState extends State<FilterFirestore> {
-  List<QueryDocumentSnapshot> data = [];
+//  ############## realtime ############
+//  Stream
+  final Stream<QuerySnapshot> usersStream =
+      //snapshots() ضفناها عشان تبقي realtime
+      FirebaseFirestore.instance.collection('users').snapshots();
 
-  intialData() async {
-    CollectionReference users = FirebaseFirestore.instance.collection("users");
-    QuerySnapshot usersdata = await users.get();
-    // await users.where("username", isNotEqualTo: "ashraf").get();
+//  List<QueryDocumentSnapshot> data = [];
 
-    // whereIn للبحث عن اكثر من عمر  & WhereNotIn  عكسها
-    //  await users.where("age", whereIn: [40, 50, 60]).get();
+  // intialData() async {
+  //   CollectionReference users = FirebaseFirestore.instance.collection("users");
 
-    // arrayContainsAny  البحث عن اكثر من قيمة
-    // await users.where("lang", arrayContainsAny:[ "fr","ar"]).get();
+  //   // await users.where("username", isNotEqualTo: "ashraf").get();
 
-    //orderBy("age", descending: false) الترتيب تصاعدي & true الترتيب تنازلي
-    // await users.orderBy("age", descending: false).get();
+  //   // whereIn للبحث عن اكثر من عمر  & WhereNotIn  عكسها
+  //   //  await users.where("age", whereIn: [40, 50, 60]).get();
 
-    //limit(2) ميعرضش غير 2 فقط من المراتبين تصاعدي
-    //  await users.orderBy("age", descending: false).limit(2).get();
+  //   // arrayContainsAny  البحث عن اكثر من قيمة
+  //   // await users.where("lang", arrayContainsAny:[ "fr","ar"]).get();
 
-    // descending: false">="- startAt([40])يعني اعرضلي بعد مترتب تصاعدي كل الاعمار اللي بتبدأ من 40 عام فيما فوق
-    //descending: false"<="- startAt([40])
-    // await users.orderBy("age", descending: false).startAt([40]).get();
+  //   //orderBy("age", descending: false) الترتيب تصاعدي & true الترتيب تنازلي
+  //   // await users.orderBy("age", descending: false).get();
 
-    //descending: false .endAt([40]) تحت او تساى 40 & true فوق 40
-    //endBefore[40] اصغر من فقط ولاتساوي  40
-    //await users.orderBy("age", descending: true).endAt([40]).get();
+  //   //limit(2) ميعرضش غير 2 فقط من المراتبين تصاعدي
+  //   //  await users.orderBy("age", descending: false).limit(2).get();
 
-    usersdata.docs.forEach(
-      (element) {
-        data.add(element);
-      },
-    );
-  }
+  //   // descending: false">="- startAt([40])يعني اعرضلي بعد مترتب تصاعدي كل الاعمار اللي بتبدأ من 40 عام فيما فوق
+  //   //descending: false"<="- startAt([40])
+  //   // await users.orderBy("age", descending: false).startAt([40]).get();
+
+  //   //descending: false .endAt([40]) تحت او تساى 40 & true فوق 40
+  //   //endBefore[40] اصغر من فقط ولاتساوي  40
+  //   //await users.orderBy("age", descending: true).endAt([40]).get();
+  //   QuerySnapshot usersdata = await users.get();
+  //   usersdata.docs.forEach(
+  //     (element) {
+  //       data.add(element);
+  //     },
+  //   );
+  // }
 
   @override
   void initState() {
-    intialData();
+    //  intialData();
 
     super.initState();
   }
@@ -53,67 +59,133 @@ class _FilterFirestoreState extends State<FilterFirestore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Filter"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
-        body: Container(
-          child: ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, i) {
-                return InkWell(
-                  //    تطبيق transaction     https://firebase.flutter.dev/docs/firestore/usage
-                  //######## مهم جدا جدا لتحويل الاموال ##############
-                  onTap: () {
-                    // اولا هنحدد doc
-                    DocumentReference documentReference = FirebaseFirestore
-                        .instance
-                        .collection('users')
-                        .doc(data[i].id);
+      appBar: AppBar(
+        title: const Text("stream"),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     //Batch write لاضافة اكثر من user في وقت واحد
+      //     CollectionReference users =
+      //         FirebaseFirestore.instance.collection('users');
+      //     DocumentReference doc1 =
+      //         FirebaseFirestore.instance.collection('users').doc("1");
+      //     DocumentReference doc2 =
+      //         FirebaseFirestore.instance.collection('users').doc("2");
 
-                    // ثانيا
-                    FirebaseFirestore.instance
-                        .runTransaction((transaction) async {
-                      // قرأه اولا واحضار doc علي الشاشة قبل الكتابة
-                      DocumentSnapshot snapshot =
-                          await transaction.get(documentReference);
-
-                      if (snapshot.exists) {
-                        var snapshotData = snapshot.data();
-
-                        if (snapshotData is Map<String, dynamic>) {
-                          var money = snapshotData['money'] + 100;
-                          transaction
-                              .update(documentReference, {"money": money});
-                        }
-                      }
-                      // بعد ما العملية تتم بنجاح اعمل ريفريش للصفحة
-                    }).then((value) {
-                      Navigator.of(context)
-                          .pushNamedAndRemoveUntil("filter", (route) => false);
-                    });
-                  },
-                  child: Card(
+      //     WriteBatch batch = FirebaseFirestore.instance.batch();
+      //     batch.set(doc1, {
+      //       "username": "pavly",
+      //       "money": 120,
+      //       "age": "20",
+      //       "phone": "2113453"
+      //     });
+      //     batch.set(doc2, {
+      //       "username": "talaat",
+      //       "money": 500,
+      //       "age": "60",
+      //       "phone": "21154653"
+      //     });
+      //     //ولايتم التنفيذ بدون كتابة الكود التالي
+      //     batch.commit();
+      //   },
+      //   child: const Icon(Icons.add),
+      // ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        // StreamBuilder نفس  futureBuilder
+        // الفرق انه اي تغيير في السرفير بيتم في نفس الوقت علي الشاشة
+        // طالما فايربيز هتبقي <QuerySnapshot>
+        child: StreamBuilder<QuerySnapshot>(
+            stream: usersStream,
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const Text("Error");
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("loading....");
+              }
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  return Card(
                     child: ListTile(
                       trailing: Text(
-                        "${data[i]['money']}\$",
-                        style: const TextStyle(
-                            color: Colors.red,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
+                        "${snapshot.data!.docs[index]['money']}\$",
+                        style: const TextStyle(fontSize: 30),
                       ),
-                      subtitle: Text("age : ${data[i]['age']}"),
+                      subtitle: Text(
+                        " age : ${snapshot.data!.docs[index]['age']}",
+                        style: const TextStyle(fontSize: 30),
+                      ),
                       title: Text(
-                        data[i]['username'],
+                        "${snapshot.data!.docs[index]['username']}",
                         style: const TextStyle(fontSize: 30),
                       ),
                     ),
-                  ),
-                );
-              }),
-        ));
+                  );
+                },
+                itemCount: snapshot.data!.docs.length,
+              );
+            }),
+      ),
+    );
   }
 }
+
+//  //    تطبيق transaction     https://firebase.flutter.dev/docs/firestore/usage
+//                   //######## مهم جدا جدا لتحويل الاموال ##############
+//                     // اولا هنحدد doc
+//                     DocumentReference documentReference = FirebaseFirestore
+//                         .instance
+//                         .collection('users')
+//                         .doc(data[i].id);
+
+//                     // ثانيا
+//                     FirebaseFirestore.instance
+//                         .runTransaction((transaction) async {
+//                       // قرأه اولا واحضار doc علي الشاشة قبل الكتابة
+//                       DocumentSnapshot snapshot =
+//                           await transaction.get(documentReference);
+
+//                       if (snapshot.exists) {
+//                         var snapshotData = snapshot.data();
+
+//                         if (snapshotData is Map<String, dynamic>) {
+//                           var money = snapshotData['money'] + 100;
+//                           transaction
+//                               .update(documentReference, {"money": money});
+//                         }
+//                       }
+//                       // بعد ما العملية تتم بنجاح اعمل ريفريش للصفحة
+//                     }).then((value) {
+//                       Navigator.of(context)
+//                           .pushNamedAndRemoveUntil("filter", (route) => false);
+//                     });
+
+//  ListView.builder(
+//               itemCount: data.length,
+//               itemBuilder: (context, i) {
+//                 return InkWell(
+//                   onTap: () {
+//                     Navigator.of(context)
+//                         .pushNamedAndRemoveUntil("filter", (route) => false);
+//                   },
+//                   child: Card(
+//                     child: ListTile(
+//                       trailing: Text(
+//                         "${data[i]['money']}\$",
+//                         style: const TextStyle(
+//                             color: Colors.red,
+//                             fontWeight: FontWeight.bold,
+//                             fontSize: 18),
+//                       ),
+//                       subtitle: Text("age : ${data[i]['age']}"),
+//                       title: Text(
+//                         data[i]['username'],
+//                         style: const TextStyle(fontSize: 30),
+//                       ),
+//                     ),
+//                   ),
+
+//                 );
+
+//               }),
